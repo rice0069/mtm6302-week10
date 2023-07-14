39d24ffc952df0b7eec5de3b4e842e5287b2b322
Marc Rice <rice0069@algonquinlive.com>
2023-07-14 11:41:27 -0400

    completes in class excercise

--- js/main.js
+++ js/main.js
@@ -48,3 +48,46 @@ const cats = [
     img: "images/kitten8.jpeg"
   }
 ]
+
+
+const catsRow = document.getElementById("catsRow")
+const cards = [
+
+]
+
+for (const cat of cats) {
+  console.log(cat.name)
+
+  // creates card HTML template copying from HTML page and replacing content with values from the object
+  const card = `
+    <div class="col">
+        <div class="card">
+            <img src="${cat.thumb}" class="card-img-top" alt="Placeholder" data-fullimg="${cat.img}" data-bs-toggle="modal" data-bs-target="#exampleModal">  
+            <div class="card-body">
+              <h5 class="class.title">${cat.name}</h5>
+              <p class="card-text">${cat.bio}</p>
+              <a href="#" class="btn btn-light">Like</a>
+            </div>
+        </div>
+    </div> <!--  col ends -->
+  `
+  // push each card template to the cards array
+  cards.push(card)
+}
+catsRow.insertAdjacentHTML("afterbegin", cards.join(""))
+
+// Selecting all the images
+const cardImages = document.querySelectorAll(".card-img-top")
+
+// Adding event listeners to multiple elements
+for (const cardImage of cardImages) {
+  cardImage.addEventListener("click", openModal)
+}
+
+function openModal () {
+  console.log(this.getAttribute("src"))
+  const fullImage = this.dataset.fullimg
+  const modalBody = document.querySelector(".modal-body")
+
+  modalBody.innerHTML = `<img src="${fullImage}" alt="placeholder">`
+}
